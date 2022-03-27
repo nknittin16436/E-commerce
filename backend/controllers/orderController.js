40481f533c0b42ab7c214b03c33adaa864259178
nknittin16436 <nknittin16436@gmail.com>
2022-03-27 19:15:11 +0530

    Backend Completed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,11 +1,12 @@
 
 const Order = require("../models/orderModel");
+const Product=require('../models/productModel');
 const ErrorHandler = require("../utils/Errorhandler");
 const catchAsyncError = require("../middleware/catchAsyncError");
 
 
 //Create New Order
-exports.newOrder=catchAsyncError(async(req,res,next)=>{
+exports.newOrder = catchAsyncError(async (req, res, next) => {
 
     const {
         shippingInfo,
@@ -16,10 +17,10 @@ exports.newOrder=catchAsyncError(async(req,res,next)=>{
         shippingPrice,
         totalPrice,
 
-    }=req.body;
+    } = req.body;
 
 
-    const order=await Order.create({
+    const order = await Order.create({
         shippingInfo,
         orderItems,
         paymentInfo,
@@ -27,38 +28,105 @@ exports.newOrder=catchAsyncError(async(req,res,next)=>{
         taxPrice,
         shippingPrice,
         totalPrice,
-        paidAt:Date.now(),
-        user:req.user._id
+        paidAt: Date.now(),
+        user: req.user._id
 
     });
-    res.status(201).json({success:true,order});
+    res.status(201).json({ success: true, order });
 });
 
 /////////////**************************************GET SINGLE ORDER ********************************************************************************************/
-exports.getSingleOrder=catchAsyncError(async (req,res,next)=>{
+exports.getSingleOrder = catchAsyncError(async (req, res, next) => {
     //populate will take user id from order and find name and email corresponding to it in user collection
     // const order=await Order.findById(req.params.id).populate("user","name email");
-    const order=await Order.findById(req.params.id).populate("user","name");
 
-    if(!order){
-       return next(new ErrorHandler("Order not fount with this Id"),404);
+    /***************************************TO DO POPULATE */
+    const order = await Order.findById(req.params.id);
+
+    if (!order) {
+        return next(new ErrorHandler("Order not fount with this Id"), 404);
     }
 
     res.status(200).json({
-        success:true,
+        success: true,
         order,
     });
-}); 
+});
 
 
 
 
 /////////////**************************************GET LOGGED IN USER ORDER ********************************************************************************************/
-exports.myOrders=catchAsyncError(async (req,res,next)=>{
-    const order =await Order.find({user:req.user._id});
+exports.myOrders = catchAsyncError(async (req, res, next) => {
+    const orders = await Order.find({ user: req.user._id });
     // const order =await Order.find();
     res.status(200).json({
-        success:true,
-        order,
+        success: true,
+        orders,
     })
 });
+
+
+
+/////////////**************************************GET ALL ORDERS-- ADMIN ********************************************************************************************/
+exports.getAllOrders = catchAsyncError(async (req, res, next) => {
+    const orders = await Order.find();
+    let totalAmount = 0;
+    orders.forEach(order => {
+        totalAmount += order.totalPrice;
+    });
+    res.status(200).json({
+        success: true,
+        totalAmount,
+        orders,
+    })
+});
+
+
+
+/////////////**************************************UPDATE ORDER STAUS -- ADMIN ********************************************************************************************/
+exports.updateOrder = catchAsyncError(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+
+    if (!order) {
+        return next(new ErrorHandler("Order not fount with this Id"), 404);
+    }
+
+    if (order.orderStatus=== "Delivered") {
+        return next(new ErrorHandler("You have already delivered this order", 400));
+    }
+    order.orderItems.forEach(async (order) => {
+        await updateStock(order.product, order.quantity);
+    })
+
+    order.orderStatus = req.body.status;
+
+    if (req.body.status === "Delivered") {
+        order.deliveredAt = Date.now();
+    }
+    await order.save({ validateBeforeSave: false });
+    res.status(200).json({
+        success: true,
+    })
+});
+/////////////**************************************DELTEE ORDER -- ADMIN ********************************************************************************************/
+exports.deleteOrder = catchAsyncError(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+    if (!order) {
+        return next(new ErrorHandler("Order not fount with this Id"), 404);
+    }
+
+    await order.remove();
+
+
+    res.status(200).json({
+        success: true,
+    })
+});
+
+//FUNCTION TO UPDATE STOCK OF PRODUCT
+async function updateStock(id, quantity) {
+    const product = await Product.findById(id);
+    product.stock -= quantity;
+    await product.save({ validateBeforeSave: false });
+} 
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const Order = require("../models/orderModel");
-const { newOrder, getSingleOrder, myOrders, myOrders1 } = require('../controllers/orderController');
+const { newOrder, getSingleOrder, myOrders, getAllOrders, updateOrder, deleteOrder} = require('../controllers/orderController');
 const { isUserAuthenticated, authorizeRoles} = require('../middleware/authentication');
 const router = express.Router();
 
 router.route("/order/new").post(isUserAuthenticated,newOrder);
+
 router.route("/order/me").get(isUserAuthenticated,myOrders);
+
 // router.route("/order/:id").get(isUserAuthenticated,authorizeRoles("admin"),getSingleOrder);
+
 router.route("/order/:id").get(isUserAuthenticated,getSingleOrder);
+
+
+router.route("/admin/orders").get(isUserAuthenticated,authorizeRoles("admin"),getAllOrders);
+router.route("/admin/order/:id").put(isUserAuthenticated,authorizeRoles("admin"),updateOrder);
+router.route("/admin/order/:id").delete(isUserAuthenticated,authorizeRoles("admin"),deleteOrder);
 
 
 
