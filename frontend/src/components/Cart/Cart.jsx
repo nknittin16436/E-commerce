73a6b177b7553c1b5a0261f84e3f3aebc210eb9c
Nand Kumar <nand.kumar@bitcs.in>
2022-09-14 16:51:25 +0530

    Total price of cart done

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -2,9 +2,9 @@ import React, { Fragment } from "react";
 import "./Cart.css";
 import CartItemCard from "./CartItemCard";
 import { useSelector, useDispatch } from "react-redux";
-import { addItemsToCart ,removeItemsFromCart} from "../../actions/cartAction";
+import { addItemsToCart, removeItemsFromCart } from "../../actions/cartAction";
 import { Typography } from "@material-ui/core";
-import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
+import RemoveShoppingCartIcon from "@mui/icons-material/RemoveShoppingCart";
 import { Link } from "react-router-dom";
 
 const Cart = () => {
@@ -13,68 +13,92 @@ const Cart = () => {
 
   const increaseQuantity = (id, quantity, stock) => {
     const newQty = quantity + 1;
-    if(stock<=quantity){
-        return;
+    if (stock <= quantity) {
+      return;
     }
-    dispatch(addItemsToCart(id,newQty));
+    dispatch(addItemsToCart(id, newQty));
   };
 
   const decreaseQuantity = (id, quantity) => {
     const newQty = quantity - 1;
-    if(1>=quantity){
-        return;
+    if (1 >= quantity) {
+      return;
     }
-    dispatch(addItemsToCart(id,newQty));
+    dispatch(addItemsToCart(id, newQty));
   };
 
-  const deleteItemFromCart=(id)=>{
+  const deleteItemFromCart = (id) => {
     dispatch(removeItemsFromCart(id));
-  }
+  };
 
   return (
-   <Fragment>
-    {cartItems.length===0?
-    <div className="emptyCart">
-        <RemoveShoppingCartIcon/>
-        <Typography>No items in Cart</Typography>
-        <Link to="/products">view products</Link>
-    </div>
-    :
     <Fragment>
-    <div className="cartPage">
-      <div className="cartHeader">
-        <p>Product</p>
-        <p>Quantity</p>
-        <p>Sub Total</p>
-      </div>
-      {cartItems &&
-        cartItems.map((item) => (
-          <div className="cartContainer" key={item.product}>
-            <CartItemCard item={item} deleteItemFromCart={deleteItemFromCart} />
-            <div className="cartInput">
-              <button onClick={()=>decreaseQuantity(item.product,item.quantity)}>-</button>
-              <span style={{ margin: "auto 10px" }}>{item.quantity}</span>
-              <button onClick={()=>increaseQuantity(item.product,item.quantity,item.stock)}>+</button>
+      {cartItems.length === 0 ? (
+        <div className="emptyCart">
+          <RemoveShoppingCartIcon />
+          <Typography>No items in Cart</Typography>
+          <Link to="/products">view products</Link>
+        </div>
+      ) : (
+        <Fragment>
+          <div className="cartPage">
+            <div className="cartHeader">
+              <p>Product</p>
+              <p>Quantity</p>
+              <p>Sub Total</p>
             </div>
-            <p className="cartSubtotal">{`₹${item.price * item.quantity}`}</p>
-          </div>
-        ))}
+            {cartItems &&
+              cartItems.map((item) => (
+                <div className="cartContainer" key={item.product}>
+                  <CartItemCard
+                    item={item}
+                    deleteItemFromCart={deleteItemFromCart}
+                  />
+                  <div className="cartInput">
+                    <button
+                      onClick={() =>
+                        decreaseQuantity(item.product, item.quantity)
+                      }
+                    >
+                      -
+                    </button>
+                    <span style={{ margin: "auto 10px" }}>{item.quantity}</span>
+                    <button
+                      onClick={() =>
+                        increaseQuantity(
+                          item.product,
+                          item.quantity,
+                          item.stock
+                        )
+                      }
+                    >
+                      +
+                    </button>
+                  </div>
+                  <p className="cartSubtotal">{`₹${
+                    item.price * item.quantity
+                  }`}</p>
+                </div>
+              ))}
 
-      <div className="cartGrossTotal">
-        <div></div>
-        <div className="cartGrossTotalBox">
-          <p>Gross Total</p>
-          <p>{`₹600`}</p>
-        </div>
-        <div></div>
-        <div className="checkOutBtn">
-          <button>Check Out</button>
-        </div>
-      </div>
-    </div>
-  </Fragment>
-}
-   </Fragment>
+            <div className="cartGrossTotal">
+              <div></div>
+              <div className="cartGrossTotalBox">
+                <p>Gross Total</p>
+                <p>{`₹${cartItems.reduce(
+                  (acc, item) => acc + item.price * item.quantity,
+                  0
+                )}`}</p>
+              </div>
+              <div></div>
+              <div className="checkOutBtn">
+                <button>Check Out</button>
+              </div>
+            </div>
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
   );
 };
 
--- frontend/src/components/layout/Header/UserOptions.js
+++ frontend/src/components/layout/Header/UserOptions.js
@@ -7,8 +7,9 @@ import DashboardCustomizeIcon from "@mui/icons-material/DashboardCustomize";
 import PersonIcon from "@mui/icons-material/Person";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import ListAltIcon from "@mui/icons-material/ListAlt";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useAlert } from "react-alert";
 
 import "./Header.css";
@@ -18,13 +19,15 @@ const UserOptions = ({ user }) => {
 
 
   const dispatch = useDispatch();
-  const alert=useAlert();
+  const alert = useAlert();
   const [open, setOpen] = useState(false);
+  const { cartItems } = useSelector((state) => state.cart);
   const navigate = useNavigate();
 
   const options = [
     { icon: <ListAltIcon />, name: "Orders", func: orders },
     { icon: <PersonIcon />, name: "Profile", func: account },
+    { icon: <ShoppingCartIcon style={{color:cartItems.length>0?"tomato":"unset"}}/>, name: `Cart(${cartItems.length})`, func: cart },
     { icon: <ExitToAppIcon />, name: "Logout", func: logout },
   ];
 
@@ -45,21 +48,24 @@ const UserOptions = ({ user }) => {
   function account() {
     navigate("/account");
   }
+  function cart() {
+    navigate("/cart");
+  }
   function logout() {
     dispatch(logoutUser());
-    
+
     alert.success("Succesfully Logged Out");
     navigate('/login');
   }
   return (
     <Fragment>
-      <Backdrop open={open} style={{zIndex:"10"}}/>
+      <Backdrop open={open} style={{ zIndex: "10" }} />
       <SpeedDial
         ariaLabel="SpeedDial tooltip example"
         onClose={() => setOpen(false)}
         onOpen={() => setOpen(true)}
         open={open}
-        style={{zIndex:"11"}}
+        style={{ zIndex: "11" }}
         direction="down"
         className="speedDial"
         // icon={<SpeedDialIcon/>}
@@ -73,10 +79,11 @@ const UserOptions = ({ user }) => {
       >
         {options.map((item) => (
           <SpeedDialAction
-          key={item.name}
+            key={item.name}
             icon={item.icon}
             tooltipTitle={item.name}
             onClick={item.func}
+            tooltipOpen={window.innerWidth <= 600 ? true : false}
           />
         ))}
       </SpeedDial>
